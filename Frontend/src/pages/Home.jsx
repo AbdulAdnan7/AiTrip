e0eb4f9c24a14c53e12c8ba40d5c31654061127b
Abdul Adnan <peerabduladnan@gmail.com>
2025-07-13 20:14:24 +0530

    Added how it works section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,14 +3,42 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import PaperPlane from '../components/PaperPlane';
 import { NavLink } from 'react-router';
+import { Sparkles, MapPin, Plane, icons, SlidersHorizontal } from 'lucide-react';
+
 
 
 
 
 const Home = () => {
+
+  const steps = [
+    {
+      title: 'Enter Trip Details',
+      description: 'Tell us where you want to go, your travel dates, budget, and interests',
+      icon: <MapPin className='w-8 h-8 text-purple-600' />
+    }, 
+    {
+      title: 'AI Plans Your Itinerary',
+      description: 'Our AI analyzes your preferences and budget to craft a personalized plan.',
+      icon: <Sparkles className='w-8 h-8 text-purple-600' />
+    },
+    {
+      title: 'Get Daily Breakdown',
+      description: 'See a day-by-day itinerary with places, food, and local experiences.',
+      icon: <Plane className='w-8 h-8 text-purple-600' />
+    },
+    {
+      title: 'Customize Your Preferences',
+      description: 'Select your travel interests, group size, and budget --- the AI tailors the plan just foryou',
+      icon: <SlidersHorizontal className='w-8 h-8 text-purple-600' />
+    }
+  ]
+
+
   return (
     <>
-      <section className="relative h-[500px] overflow-hidden bg-slate-100 px-4 py-20">
+    {/** For Hero Section */}
+      <section className="relative h-[500px] overflow-hidden bg-white px-4 py-20">
 
 
         <div className=' z-100 inset-0'>
@@ -40,12 +68,33 @@ const Home = () => {
         </div>
       </section>
 
-      <section>
-        <div>
-          <h1></h1>
+{/** For How it Works Section */}
+      <section className='bg-white shadow-md h-[400px]'>
+        <div className='flex justify-center mt-4'>
+          <h1 className='text-2xl font-semibold'>How it Works</h1>
         </div>
+        <div className='grid md:grid-cols-2 lg:grids-cols-4 gap-6 max-w-6xl mx-auto'>
+          {
+            steps.map((step, index) => (
+              <div key={index} className='bg-white rounded-2xl p-6 shadow-md hover:shadow-lg transition-all space-y-3 text-center'>
+                <div className='flex justify-center'>
+                  {step.icon}
+                </div>
+                  <h3 className='text-lg font-semibold'>{step.title}</h3>
+                  <p className='text-sm text-gray-600'>{step.description}</p>
+                </div>
+            ))
+
+          }
+        </div>
+
+       
       </section>
 
+      {/** For Testimonals */}
+        <section className='bg-white shadow-md h-[400px]'>
+
+        </section>
     </>
   );
 };
